b93f10e7fb41d6b10992fcef36686437e90b49e4
qweq05293 <qweq05293@gmail.com>
2026-04-08 19:15:22 +0000

    fix build

--- apps/back/src/modules/user/user.service.ts
+++ apps/back/src/modules/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import { User } from "../generated/client.ts";
 import { PrismaService } from "../prisma/prisma.service";
+import { User } from "../../generated/client";
 
 @Injectable()
 export class UserService {
